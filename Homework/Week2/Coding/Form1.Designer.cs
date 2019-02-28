18d450cfc45d43cac56b3f20c7f3daaa8c96e511
HollowMan6 <jiangsl18@lzu.edu.cn>
2019-02-28 22:14:31 +0800

    Latest push

---
+++ Homework/Week2/Coding/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnJudge = new System.Windows.Forms.Button();
+            this.lblOp = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstDisp = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(63, 77);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(0, 15);
+            this.lblA.TabIndex = 0;
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(279, 74);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(66, 25);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(324, 1);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(85, 31);
+            this.btnNew.TabIndex = 3;
+            this.btnNew.Text = "重出一题";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnJudge
+            // 
+            this.btnJudge.Location = new System.Drawing.Point(167, 144);
+            this.btnJudge.Name = "btnJudge";
+            this.btnJudge.Size = new System.Drawing.Size(81, 31);
+            this.btnJudge.TabIndex = 4;
+            this.btnJudge.Text = "提交";
+            this.btnJudge.UseVisualStyleBackColor = true;
+            this.btnJudge.Click += new System.EventHandler(this.BtnJudge_Click_1);
+            // 
+            // lblOp
+            // 
+            this.lblOp.AutoSize = true;
+            this.lblOp.Location = new System.Drawing.Point(113, 77);
+            this.lblOp.Name = "lblOp";
+            this.lblOp.Size = new System.Drawing.Size(0, 15);
+            this.lblOp.TabIndex = 5;
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(164, 77);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(0, 15);
+            this.lblB.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(235, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "=";
+            // 
+            // lstDisp
+            // 
+            this.lstDisp.FormattingEnabled = true;
+            this.lstDisp.ItemHeight = 15;
+            this.lstDisp.Location = new System.Drawing.Point(55, 209);
+            this.lstDisp.Name = "lstDisp";
+            this.lstDisp.Size = new System.Drawing.Size(309, 199);
+            this.lstDisp.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "得分：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(364, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 11;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(184, 10);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 19);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "自动出题";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 6000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "每隔";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(23, 25);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.Text = "6";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(156, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "秒";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(58, 191);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 15);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "结果：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(458, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstDisp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblOp);
+            this.Controls.Add(this.btnJudge);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblA);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnJudge;
+        private System.Windows.Forms.Label lblOp;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstDisp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Homework/Week2/Coding/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        
+        int a, b, grade = 0;
+        string op;
+        int result;
+        Random rnd = new Random();
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked== true)
+                timer1.Enabled = true;
+            else
+                timer1.Enabled = true;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+            BtnJudge_Click_1(null, null);
+        }
+
+        private void btnNew_Click(object sender, System.EventArgs e)
+        {
+            a = rnd.Next(9) + 1;
+            b = rnd.Next(9) + 1;
+            int c = rnd.Next(4);
+            switch (c)
+            {
+                case 0: op = "+"; result = a + b; break;
+                case 1: op = "-"; result = a - b; break;
+                case 2: op = "*"; result = a * b; break;
+                case 3: op = "/"; result = a / b; break;
+            }
+            lblA.Text = a.ToString();
+            lblB.Text = b.ToString();
+            lblOp.Text = op;
+            txtAnswer.Text = "";
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = int.Parse(textBox1.Text)*1000;
+        }
+
+        private void BtnJudge_Click_1(object sender, EventArgs e)
+        {
+            string str = txtAnswer.Text;
+            if (str == "")
+                MessageBox.Show("请输入答案！");
+            else
+            {
+                double d = double.Parse(str);
+                string disp = "" + a + op + b + "=" + str + " ";
+                if (d == result) // if( Math.Abs(d-result)< 1e-3 )
+                {
+                    disp += "✔";
+                    grade++;
+                    label2.Text = grade.ToString();
+                    btnNew_Click(null, null);
+                    label7.BackColor = Color.Green;
+                }
+                else
+                {
+                    disp += "✘";
+                    btnNew_Click(null, null);
+                    label7.BackColor = Color.Red;
+                }
+                lstDisp.Items.Add(disp);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            btnNew_Click(null, null);
+        }
+    }
+}
