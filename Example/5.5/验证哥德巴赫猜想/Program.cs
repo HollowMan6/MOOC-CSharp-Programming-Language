2607a085b9eba85646ad1925851cdfc6c9e50c53
HollowMan6 <jiangsl18@lzu.edu.cn>
2019-03-18 19:37:07 +0800

    Latest push

---
+++ Example/5.5/验证哥德巴赫猜想/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class GDBH 
+{
+      
+   static void Main()
+   {
+        for (int n = 6; n <= 100; n+=2)
+        {
+            for (int a = 3; a<=n/2; a+=2)
+            {
+                int b = n - a;
+                if (IsPrime(a) && IsPrime(b))
+                {
+                    Console.WriteLine(n + "=" + a + "+" + b);
+                    break;
+                }
+            }
+        }
+    }
+  
+    static bool IsPrime(int a)
+    {
+        for (int i = 2; i <= a / 2; i++)
+        {
+            if (a % i == 0) return false;
+        }
+        return true;
+    }
+}
